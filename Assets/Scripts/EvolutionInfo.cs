c1f8048d3f9ab1568ffb44d3db90d3713d9ba290
mista makzim <56073641+ZaroTV@users.noreply.github.com>
2020-12-19 21:59:05 +0100

    Added evolution info & started making evolution menu

---
+++ Assets/Scripts/EvolutionInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvolutionInfo : MonoBehaviour
+{
+    public string Name;
+    public int Cost;
+    public int Damage;
+    public int Defense;
+    public int FireRate;
+    public bool IgnoresPlating;
+}
