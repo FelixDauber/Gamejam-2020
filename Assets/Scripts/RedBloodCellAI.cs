924c5b7bd75bbf82b996e04bf6ca8c8de23b44d4
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2020-12-19 15:31:25 +0100

    adds RedBloodCellAI wandering

--- Assets/Scripts/RedBloodCellAI.cs
+++ Assets/Scripts/RedBloodCellAI.cs
@@ -6,10 +6,12 @@ public class RedBloodCellAI : MonoBehaviour
 {
     CellMovement cellMovement;
     public float escapeRange;
+    public float wanderRange = 15;
 
     private void Start()
     {
         cellMovement = GetComponent<CellMovement>();
+        cellMovement.targetPoint = transform.position;
     }
 
     void Update()
@@ -18,6 +20,10 @@ public class RedBloodCellAI : MonoBehaviour
         {
             //cellMovement.targetPoint = transform.position - PlayerController.playerController.transform.position;
             cellMovement.targetPoint = transform.position - PlayerController.playerController.transform.position + transform.position;
+        }
+        else if(Vector2.Distance(cellMovement.targetPoint, transform.position) < 0.3f)
+        {
+            cellMovement.targetPoint += new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * wanderRange;
         }
     }
 }
