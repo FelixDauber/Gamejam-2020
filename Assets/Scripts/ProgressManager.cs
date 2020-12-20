9b7b0b2cd8e56c37f2592ad30824fa66833a0d81
mista makzim <56073641+ZaroTV@users.noreply.github.com>
2020-12-20 12:46:19 +0100

    Added color changing script

---
+++ Assets/Scripts/ProgressManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressManager : MonoBehaviour
+{
+    public Color backgroundColor;
+    public GameObject background;
+    private void Start(){
+        backgroundColor = Color.black;
+    }
+    public void ChangeColor(){
+        
+    }
+}
